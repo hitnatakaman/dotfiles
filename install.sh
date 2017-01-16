88ad5f85e766bd0165e0678cc0ee66ddb3f38666
hitnatakaman <you@example.com>
2017-01-16 14:22:03 +0900

    install.sh update

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
 dein_dir=~/.vim/dein/repos/github.com/Shougo/dein.vim
 if [ -d $dein_dir ]; then
-	git clone https://github.com/Shougo/dein.vim ~/.vim/dein/repos/github.com/Shougo/dein.vim
+	git clone https://github.com/Shougo/dein.vim $dein_dir
 fi
 
 ln -sf ~/dotfiles/dein/dein.toml ~/.vim/dein/dein.toml
