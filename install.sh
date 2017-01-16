19293bcbbb4478a1cb86c80fe912df3688cfdcc4
hitnatakaman <you@example.com>
2017-01-16 14:31:58 +0900

    install.sh update

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/bash
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-
 dein_dir=~/.vim/dein/repos/github.com/Shougo/dein.vim
-if [ -d $dein_dir ]; then
+if [ ! -d $dein_dir ]; then
 	git clone https://github.com/Shougo/dein.vim $dein_dir
 fi
 
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/dein/dein.toml ~/.vim/dein/dein.toml
 ln -sf ~/dotfiles/dein/dein_lazy.toml ~/.vim/dein/dein_lazy.toml
